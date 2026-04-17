6e61e6bfee3c4497fc07a7f43bab12b9d4fc2aca
Rudra Patel <patel.rudra4@northeastern.edu>
2026-04-17 10:12:28 -0400

    added a select box for status and severity in the error logs page

--- app/src/pages/32_Error_Logs.py
+++ app/src/pages/32_Error_Logs.py
@@ -17,3 +17,17 @@ st.markdown("""
         }
     </style>
 """, unsafe_allow_html=True)
+
+st.title("Error Logs")
+st.write("View recent system errors and failed operations")
+
+col1, col2 = st.columns(2)
+
+status_option = ["All", "Resolved", "Unresolved"]
+severity_option = ["All", "Low", "Medium", "High"]
+
+with col1:
+    status_filter = st.selectbox("Filter by Status", options=status_option)
+
+with col2:
+    severity_filter = st.selectbox("Filter by Severity", options=severity_option)
