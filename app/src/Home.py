296c684559d862fd1b19053a7ef22f56f53eb34d
amogh-reddy21 <peddapothla.a@northeastern.edu>
2026-04-19 14:42:22 -0400

    Resolve merge conflicts for recruiter persona

--- app/src/Home.py
+++ app/src/Home.py
@@ -70,7 +70,7 @@ if st.button('Act as System Administrator',
     st.session_state['authenticated'] = True
     st.session_state['role'] = 'administrator'
     st.session_state['first_name'] = 'SysAdmin'
-    st.switch_page('pages/20_Admin_Home.py')
+    st.switch_page('pages/30_Admin_Dashboard.py')
 
 if st.button('Act as Reece James, a Recruiter',
              type='primary',
@@ -79,3 +79,13 @@ if st.button('Act as Reece James, a Recruiter',
     st.session_state['role'] = 'recruiter'
     st.session_state['first_name'] = 'Reece'
     st.switch_page('pages/90_Recruiter_Home.py')
+
+if st.button('Act as Alex, a Job Seeker',
+             type='primary',
+             use_container_width=True):
+    st.session_state['authenticated'] = True
+    st.session_state['role'] = 'job_seeker'
+    st.session_state['first_name'] = 'Alex'
+    st.session_state['student_id'] = 1
+    logger.info("Logging in as Job Seeker Persona")
+    st.switch_page('pages/40_Job_Seeker_Home.py')
