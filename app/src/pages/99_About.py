74c4d2f96d56c8b957a1d0ef740f6ab44e1bdf30
VKumar2905 <kumar.van@northeastern.edu>
2026-04-19 13:31:43 -0400

    Revert "Analytics Personsa"
    This reverts commit 1576f642452c31baa8e383b12b3dbbc56c47ae78.

--- app/src/pages/99_About.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from modules.nav import SideBarLinks
-
-SideBarLinks()
-
-st.title("About OfferWatch")
-st.write("OfferWatch is a job and internship tracking platform.")
-st.write("This page is under construction.")
