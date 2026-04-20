6b0c6d51e03b3172b35e200d5c4b6aff862bcb57
Rudra Patel <patel.rudra4@northeastern.edu>
2026-04-19 18:01:28 -0600

    added logo to sidebar

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -200,7 +200,7 @@ def SideBarLinks(show_home=False):
     if "authenticated" in st.session_state and st.session_state.get("role") == "administrator":
         st.sidebar.image("assets/OfferWatchAdminLogo.png", width=150)
     else:
-        st.sidebar.image("assets/logo.png", width=150)
+        st.sidebar.image("assets/OfferWatch.png", width=250)
 
     if "authenticated" not in st.session_state:
         st.session_state["authenticated"] = False
