4393b4630639d603ed7974cbbb3cb5e2001b1bf5
Ansh Vats <anshvats12345@gmail.com>
2026-04-20 22:36:00 -0400

    Pulled changes onto my local machine

--- app/src/pages/31_User_Management_.py
+++ app/src/pages/31_User_Management_.py
@@ -111,7 +111,11 @@ with col1:
     update_id = st.number_input("User ID to Update", min_value=1, step=1)
     new_role_id = st.selectbox("New Role", options=[1, 2, 3, 4],format_func=role_options.get)
 with col2:
+<<<<<<< HEAD
     new_institution_id = st.selectbox("New Institution", options=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],format_func=institution_options.get)
+=======
+    new_institution_id = st.selectbox("New Institution", options=[1, 2, 3, 4],format_func=institution_options.get)
+>>>>>>> 1e42fa0 (Pulled changes onto my local machine)
 
 if st.button("Update User", type="primary"):
     update_input = {
